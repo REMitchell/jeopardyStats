953426a2dbeefa0ce4c7d32a1f4fd7bf3b27f9ae
Ryan Mitchell <ryan@Ryans-MacBook-Air.local>
2016-05-16 22:46:35 -0400

    Almost done, takes in two params, sets appropriate JSON data

--- jeopardy/src/com/ryanemitchell/jeopardy/Jeopardy.java
+++ jeopardy/src/com/ryanemitchell/jeopardy/Jeopardy.java
@@ -17,29 +17,59 @@ import com.ryanemitchell.entities.Game;
 import com.ryanemitchell.entities.Score;
 import com.ryanemitchell.util.StatsUtil;
 
+/**
+ * This class is primarily responsible for retrieving the user input variables to produce
+ * the Jeopardy T Test data, and returning a JSON array of test results
+ * @author ryan
+ *
+ */
 public class Jeopardy extends HttpServlet implements Servlet {
-       
+    private final String DEFAULT_TESTSCORE = "final";
+    private final double DEFAULT_ALPHA = .05;
+	
     public Jeopardy() {}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        String pageTitle = "My Page Title";
-        request.setAttribute("title", pageTitle);
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
-        
+    	ObjectMapper mapper = new ObjectMapper();
+    	System.out.println(mapper.writeValueAsString(request.getParameterMap()));
+    	double alpha = DEFAULT_ALPHA;
+    	String testScore = DEFAULT_TESTSCORE;
+    	
+    	if(request.getParameter("alpha") != null) {
+    		alpha = Double.valueOf(request.getParameter("alpha"));
+    		System.out.println("Alpha is: "+alpha);
+    	}
+    	
+    	if(request.getParameter("testScore") != null) {
+    		testScore = request.getParameter("testScore");
+    	}
+    	
+    	
         ScoreDao scoreDao = ScoreDao.getInstance();
         Map<Integer, List<Score>> scoreMap = scoreDao.getScoreMap();
-        boolean[][] testResults = new boolean[10][10];
+        Boolean[][] testResults = new Boolean[10][10];
         
         
         //Iterate through each set of scores, against each other
         for(int first = 1; first <= 10; first++) {
+        	//Where the groups are the same, the null hypothesis is obviously accepted,
+        	//and running the test doesn't really make sense
+        	testResults[first-1][first-1] = false;
         	for(int second = first + 1; second <= 10; second ++) {
-        		testResults[first][second] = StatsUtil.tTest(scoreMap.get(first), scoreMap.get(second), .95);
+        		Boolean testResult = StatsUtil.tTest(scoreMap.get(first), scoreMap.get(second), alpha, testScore);
+        		//This is symmetric along the diagonal
+        		testResults[first-1][second-1] = testResult;
+        		testResults[second-1][first-1] = testResult;
         	}
         }
         
-        ObjectMapper mapper = new ObjectMapper();
-        request.setAttribute("statsJson", mapper.writeValueAsString(testResults));
+        String result = mapper.writeValueAsString(testResults);
+        System.out.println("Results are:");
+        System.out.print(result);
+        String pageTitle = "My Page Title";
+        request.setAttribute("title", pageTitle);
+        request.setAttribute("stats", result);
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
