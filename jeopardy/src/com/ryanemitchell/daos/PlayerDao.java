3b2a20aa9dfdd1794fe414180d2d3581fc13bc07
Ryan Mitchell <ryan@Ryans-MacBook-Air.local>
2016-05-17 01:07:01 -0400

    Removed uneeded imports

--- jeopardy/src/com/ryanemitchell/daos/PlayerDao.java
+++ jeopardy/src/com/ryanemitchell/daos/PlayerDao.java
@@ -4,10 +4,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import com.ryanemitchell.entities.Game;
 import com.ryanemitchell.entities.Player;
 import com.ryanemitchell.exception.ServerException;
 
