08657d21ddc09fe360d0f1f3f6f900c962616fa1
Ryan Mitchell <ryan@Ryans-MacBook-Air.local>
2016-05-16 22:45:24 -0400

    Added t test util

--- jeopardy/src/com/ryanemitchell/util/StatsUtil.java
+++ jeopardy/src/com/ryanemitchell/util/StatsUtil.java
@@ -2,12 +2,12 @@ package com.ryanemitchell.util;
 
 import java.util.List;
 import org.apache.commons.math3.stat.inference.TTest;
-
+import com.fasterxml.jackson.annotation.JsonView;
 import com.ryanemitchell.entities.Score;
+import com.ryanemitchell.exception.ClientException;
 
 public class StatsUtil {
 
-	
 	public static boolean tTest(List<Score> scoreObjs1, List<Score> scoreObjs2, Double alpha) {
 		double[] scores1 = new double[scoreObjs1.size()];
 		double[] scores2 = new double[scoreObjs2.size()];
@@ -20,4 +20,54 @@ public class StatsUtil {
  		TTest tester = new TTest();
  		return tester.homoscedasticTTest(scores1,scores2, alpha);
 	}
+	
+	public static boolean tTest(List<Score> scoreObjs1, List<Score> scoreObjs2, Double alpha, String testScore) throws ClientException {
+		double[] scores1 = new double[scoreObjs1.size()];
+		double[] scores2 = new double[scoreObjs2.size()];
+		
+		if(testScore.equalsIgnoreCase("FINAL")) {
+			for(int i = 0; i < scoreObjs1.size(); i++) {
+	 			scores1[i] = (double)scoreObjs1.get(i).getFinalScore();
+	 		}
+	 		for(int i = 0; i < scoreObjs2.size(); i++) {
+	 			scores2[i] = (double)scoreObjs2.get(i).getFinalScore();
+	 		}
+			
+		} else if(testScore.equalsIgnoreCase("CORYAT")) {
+			for(int i = 0; i < scoreObjs1.size(); i++) {
+	 			scores1[i] = (double)scoreObjs1.get(i).getCoryat();
+	 		}
+	 		for(int i = 0; i < scoreObjs2.size(); i++) {
+	 			scores2[i] = (double)scoreObjs2.get(i).getCoryat();
+	 		}
+		} else if(testScore.equalsIgnoreCase("BREAKSCORE")) {
+			for(int i = 0; i < scoreObjs1.size(); i++) {
+	 			scores1[i] = (double)scoreObjs1.get(i).getBreakScore();
+	 		}
+	 		for(int i = 0; i < scoreObjs2.size(); i++) {
+	 			scores2[i] = (double)scoreObjs2.get(i).getBreakScore();
+	 		}
+		} else if(testScore.equalsIgnoreCase("ROUND1")) {
+			for(int i = 0; i < scoreObjs1.size(); i++) {
+	 			scores1[i] = (double)scoreObjs1.get(i).getRound1();
+	 		}
+	 		for(int i = 0; i < scoreObjs2.size(); i++) {
+	 			scores2[i] = (double)scoreObjs2.get(i).getRound1();
+	 		}
+			
+		} else if(testScore.equalsIgnoreCase("ROUND2")) {
+			for(int i = 0; i < scoreObjs1.size(); i++) {
+	 			scores1[i] = (double)scoreObjs1.get(i).getRound2();
+	 		}
+	 		for(int i = 0; i < scoreObjs2.size(); i++) {
+	 			scores2[i] = (double)scoreObjs2.get(i).getRound2();
+	 		}
+		} else {
+			throw new ClientException("Invalid score type: "+testScore);
+		}
+		
+		TTest tester = new TTest();
+ 		return tester.homoscedasticTTest(scores1,scores2, alpha);
+	}
+	
 }
